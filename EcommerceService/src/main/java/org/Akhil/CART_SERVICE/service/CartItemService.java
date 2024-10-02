cfdaaedd864bd7e36402982cbef4075d3ca6433f
Akhil351 <akhil.vathaluru@gmail.com>
2024-10-02 22:55:35 +0530

    shop-microservice-project

---
+++ EcommerceService/src/main/java/org/Akhil/CART_SERVICE/service/CartItemService.java
@@ -0,0 +1,10 @@
+package org.Akhil.CART_SERVICE.service;
+
+import org.Akhil.COMMON_SERVICE.model.CartItem;
+
+public interface CartItemService {
+    void addItemToCart(Long cartId,Long productId,int quantity);
+    void removeItemFromCart(Long cartId,Long productId);
+    void updateItemQuantity(Long cartId,Long productId,int quantity);
+    CartItem getCartItem(Long cartId,Long productId);
+}
