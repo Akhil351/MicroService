4ed39f3046ce911a8c75ac13ebe887632c802f35
Akhil351 <akhil.vathaluru@gmail.com>
2024-10-05 23:07:54 +0530

    changes

---
+++ login-service/src/test/java/org/Akhil/login/LoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.Akhil.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
