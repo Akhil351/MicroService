10531ba78d5c672b551f3eae2490996b272eea20
Akhil351 <akhil.vathaluru@gmail.com>
2024-10-19 00:11:13 +0530

    correct api gateway

--- login-service/src/main/java/org/Akhil/login/LoginServiceApplication.java
+++ login-service/src/main/java/org/Akhil/login/LoginServiceApplication.java
@@ -3,13 +3,14 @@ package org.Akhil.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"org.Akhil.common","org.Akhil.login"})
-//@EntityScan({"org.Akhil.common.model"})
+@EntityScan({"org.Akhil.common.model"})
 @EnableMongoRepositories({"org.Akhil.common.repo"})
 @EnableFeignClients
 public class LoginServiceApplication {
