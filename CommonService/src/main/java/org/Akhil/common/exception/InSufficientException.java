4a6c3f5ec08bb4dbe71936a3063008b400e97591
Akhil351 <akhil.vathaluru@gmail.com>
2024-11-06 21:40:01 +0530

    add one new exception class

---
+++ CommonService/src/main/java/org/Akhil/common/exception/InSufficientException.java
@@ -0,0 +1,7 @@
+package org.Akhil.common.exception;
+
+public class InSufficientException extends RuntimeException {
+    public InSufficientException(String message) {
+        super(message);
+    }
+}
--- login-service/src/main/java/org/Akhil/login/service/WalletService.java
+++
@@ -1,7 +0,0 @@
-package org.Akhil.login.service;
-
-import org.Akhil.common.model.Wallet;
-
-public interface WalletService {
-    Wallet addWallet();
-}
