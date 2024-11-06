4c47994e28c0d87919463f1f9715d1b1e683b48e
akhileswarv <akhileswarv@ideyalabs.com>
2024-11-06 10:39:11 +0530

    added bean for UserContextRequest

--- CommonService/src/main/java/org/Akhil/common/config/security/SecurityValidate.java
+++ CommonService/src/main/java/org/Akhil/common/config/security/SecurityValidate.java
@@ -1,5 +1,6 @@
 package org.Akhil.common.config.security;
 
+import org.Akhil.common.enums.Role;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,6 @@ public class SecurityValidate {
     public boolean isAdmin(){
         Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
         return  authentication.getAuthorities().stream()
-                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals("Admin"));
+                .anyMatch(grantedAuthority -> grantedAuthority.getAuthority().equals(Role.ADMIN.getRole()));
     }
 }
